f515b90f2adbd4715dc3417139df6561fe82656c
cbhavesh87 <cbhavesh87.gmail.com>
2026-05-17 16:32:58 +0000

    added 2 lines

--- sample.sh
+++ sample.sh
@@ -1,3 +1,5 @@
 this is sample file
 this we will use for testing
 added 3rd line for testing
+Git checkout --->>> to revert the changes in local directory (working area)
+Git reset --->>>> to revert changes from commit area to working area
