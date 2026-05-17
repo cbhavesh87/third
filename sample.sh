5db67d556b523f53b201613c6826df2134d9b7c9
cbhavesh87 <cbhavesh87.gmail.com>
2026-05-17 16:35:23 +0000

    Revert "added 2 lines"
    This reverts commit f515b90f2adbd4715dc3417139df6561fe82656c.

--- sample.sh
+++ sample.sh
@@ -1,5 +1,3 @@
 this is sample file
 this we will use for testing
 added 3rd line for testing
-Git checkout --->>> to revert the changes in local directory (working area)
-Git reset --->>>> to revert changes from commit area to working area
